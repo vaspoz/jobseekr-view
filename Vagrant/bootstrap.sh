288191d5fe87d458922d85d9d5afd0ad1100e817
Vasilii Pozdeev <Vasilii_Pozdeev@epam.com>
2016-07-13 09:22:02 +0200

    Add to Vagrant VB gulp-cli and packages from package.json

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -5,3 +5,11 @@ curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g npm
 sudo apt-get install -y git
+
+# Building
+cd /home/vagrant/shared/ && sudo npm install --no-bin-links
+sudo npm cache clear
+sudo npm install -g gulp-cli
+
+# Run application
+cd /home/vagrant/shared/ && npm run serve
