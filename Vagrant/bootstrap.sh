0a44319667c08bf82fb02c0a078744a3a0781290
Vasilii Pozdeev <Vasilii_Pozdeev@epam.com>
2016-07-14 10:00:12 +0200

    Simplify npm usage

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Colorful bash
+sed -i.bak 's/\#force_color_prompt=yes/force_color_prompt=yes/' /home/vagrant/.bashrc
+
+# Setup environment
 sudo apt-get update -y
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g npm
 sudo apt-get install -y git
+echo 'bin-links=false' > .npmrc
 
 # Building
-cd /home/vagrant/shared/ && sudo npm install --no-bin-links
+cd /home/vagrant/shared/ && sudo npm install
 sudo npm cache clear
 sudo npm install -g gulp-cli
 
