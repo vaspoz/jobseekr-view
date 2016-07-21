ac6fb684d0892c7c16546008d923b33631db4f37
Vasilii Pozdeev <Vasilii_Pozdeev@epam.com>
2016-07-21 08:33:48 +0200

    Rewrite gulp tasks using browserify, watch and connect
    Browserify compile and bundle all modules to one file.
    Actually to 2 files - 1 for vendors and 1 for self code. Vendors compile much
    longer

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,61 @@
+'use strict';
+
+var browserify = require('browserify');
 var gulp = require('gulp');
-var babel = require('gulp-babel');
-var watch = require('gulp-watch');
+var source = require('vinyl-source-stream');
 var connect = require('gulp-connect');
+
+// Update this list when you add new import in your source
+var vendors = [
+    'react',
+    'react-dom'
+];
+
+var appBundle = browserify('./src/render.js');
+
+gulp.task('index:copy', function () {
+    return gulp.src('./index.html')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('bundle:vendors', function () {
+    var stream = browserify({
+        require: vendors
+    })
+        .bundle()
+        .pipe(source('vendors.js'))
+        .pipe(gulp.dest('./dist/'))
+
+    vendors.forEach(function (dep) {
+        appBundle.external(dep);
+    });
+
+    return stream;
+});
+
+gulp.task('bundle', function () {
+    return appBundle
+        .transform('babelify', {presets: ['es2015', 'react']})
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('connect', function () {
     "use strict";
     connect.server({
-        root: './',
+        root: './dist',
         port: 3000
     });
 });
 
-// If js change, compile it using babel:
-// 1. ES6 -> ES5
-// 2. JSX -> JS
-gulp.task('js-watch', function () {
-    "use strict";
-    return gulp.src('src/**/*.js*')
-        .pipe(watch('src/**/*.js*', {usePolling: true}))
-        .pipe(babel())
-        .pipe(gulp.dest('target'))
-        .pipe(connect.reload());
+gulp.task('reload', ['bundle'], function () {
+    connect.reload();
 });
 
-gulp.task('default', ['connect', 'js-watch']);
+gulp.task('default', ['index:copy', 'bundle:vendors', 'bundle', 'connect'], function () {
+    gulp.watch('./src/*.js', ['bundle', 'reload'])
+        .on('change', function (event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        });
+});
